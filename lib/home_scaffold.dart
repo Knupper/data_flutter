8e4075b35af8a6b838376aa849215cebeaf4dddf
Max Steffen <max.steffen@mail.com>
2024-12-09 15:15:05 +0100

    add task2

---
+++ lib/home_scaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// Aufgabe 2:
+// Unten stehende Karteikarte nachbauen, Bild kann durch Placeholder ersetzt werden
+// https://img.freepik.com/vektoren-premium/farbige-profilkarte-mit-einem-benutzer-symbol-und-einzelheiten_1294240-68100.jpg?w=1800
+class HomeScaffold extends StatelessWidget {
+  const HomeScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_schulung/home_scaffold.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const HomeScaffold(),
+    );
   }
 }
