090043346a097c3df87a070da791b9a840c2612c
Max Steffen <max.steffen@mail.com>
2024-12-11 09:36:22 +0100

    add mock mode

--- lib/data/data_sources/advice_remote_data_source.dart
+++ lib/data/data_sources/advice_remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter_schulung/data/dtos/advice_dto.dart';
 import 'package:flutter_schulung/data/exceptions/exceptions.dart';
@@ -28,5 +29,24 @@ class AdviceRestDataSource extends AdviceRemoteDataSource {
       final responseBody = json.decode(response.body);
       return AdviceDto.fromJson(responseBody);
     }
+  }
+}
+
+class AdviceMockDataSource extends AdviceRemoteDataSource {
+  AdviceMockDataSource();
+
+  final List<AdviceDto> _mockData = [
+    AdviceDto(id: 1, advice: 'Hallo Welt'),
+    AdviceDto(id: 2, advice: 'Hallo World'),
+    AdviceDto(id: 3, advice: 'Hallo Baum'),
+    AdviceDto(id: 4, advice: 'Hallo Sonne'),
+    AdviceDto(id: 5, advice: 'Hallo Mond'),
+  ];
+
+  @override
+  Future<AdviceDto> getAdvice({String id = ''}) async {
+    final randomId = Random().nextInt(5);
+
+    return _mockData[randomId];
   }
 }
---
+++ lib/main_mock.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_schulung/data/data_sources/advice_remote_data_source.dart';
+import 'package:flutter_schulung/data/repositories/advice_repo_impl.dart';
+import 'package:flutter_schulung/domain/repositories/advice_repo.dart';
+import 'package:flutter_schulung/my_app.dart';
+
+void main() {
+  runApp(RepositoryProvider<AdviceRepo>(
+    create: (context) => AdviceRepoImpl(
+      dataSource: AdviceMockDataSource(),
+    ),
+    child: const MyApp(),
+  ));
+}
